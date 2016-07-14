145375656d2ae66f58f1d7530ae4d05a98d4bc80
Tomasz Dudzik <Tomasz_Dudzik@epam.com>
2016-07-14 11:47:39 +0200

    Modify MainLauncher in order to use GameEngine and launch the game.

--- src/main/java/io/github/expansionteam/battleships/MainLauncher.java
+++ src/main/java/io/github/expansionteam/battleships/MainLauncher.java
@@ -1,7 +1,13 @@
 package io.github.expansionteam.battleships;
 
+import io.github.expansionteam.battleships.engine.game.GameEngine;
+import io.github.expansionteam.battleships.ui.ConsoleOutputOperations;
+
 public class MainLauncher {
+
     public static void main(String[] args) {
-        System.out.println("Testing.");
+        GameEngine gameEngine = new GameEngine(new ConsoleOutputOperations());
+        gameEngine.launch();
     }
+    
 }
